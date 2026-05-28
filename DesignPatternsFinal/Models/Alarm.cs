5372c4299295b3ec2440e2a3e939aece615b4d36
SmartDude18 <alexanderh1027@gmail.com>
2026-05-27 18:07:02 -0600

    observer pattern

---
+++ DesignPatternsFinal/Models/Alarm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsFinal.Models
+{
+    internal interface Alarm
+    {
+        public void newMinute(DateTime newTime);
+    }
+}
